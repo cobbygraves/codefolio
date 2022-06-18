d086365047ef2ebb13c6bb9ed080ee0dc070d149
cobbygraves <rmeocnis@hotmail.com>
2022-06-18 06:33:54 +0100

    remove comma in portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -30,7 +30,7 @@ const socialMediaLinks = {
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
-};
+}
 
 // Skills Section
 
